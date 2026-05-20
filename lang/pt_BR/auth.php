03ac9c5e49581cf19328be9a956797c2bebceaf4
sdronsh <sdronsh@gmail.com>
2026-05-20 17:18:43 -0300

    Trducao autenticacao

---
+++ lang/pt_BR/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'As credenciais informadas nao conferem.',
+    'password' => 'A senha informada esta incorreta.',
+    'throttle' => 'Muitas tentativas de acesso. Tente novamente em :seconds segundos.',
+];
---
+++ resources/lang/pt_BR/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Sua senha foi redefinida.',
+    'sent' => 'Enviamos o link de redefinicao de senha para seu e-mail.',
+    'throttled' => 'Aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinicao de senha e invalido.',
+    'user' => 'Nao encontramos um usuario com esse e-mail.',
+];
